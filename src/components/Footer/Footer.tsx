60a7af4e94fd3ac674d5b7bb21fcc9a55ee456b2
Jeremy Díaz <jeremy664k@gmail.com>
2023-03-05 21:44:08 -0300

    Update footer with NavItems and status

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,31 +1,33 @@
-import { FavIcon } from "../../elements/Icons";
-import { type LinkTypes } from "../../types";
-import { navLinks } from "../../mocks/navLinks";
+import { CircleIcon, FavIcon } from "../../elements/Icons";
 
 import Link from "../../elements/Link";
-import NavLink from "../Header/NavLink";
+import NavItems from "../Header/NavItems";
 import ContactSocial from "../Contact/ContactSocial";
 
 export const Footer = () => {
     return (
-        <footer className="mt-28 border-t-2 border-t-gray-400">
+        <footer className="mt-28 border-t-2 border-t-gray-400/20 text-base">
             <div className="max-w-screen-xl mx-auto pt-28 p-4 flex justify-between">
-                <div>
-                    <Link href="/" title="Icono de Jeremy Díaz" removeSpacing>
-                        <FavIcon />
-                    </Link>
-                </div>
                 <div className="flex flex-col gap-8 items-start">
+                    <div>
+                        <Link href="/" title="Icono de Jeremy Díaz" removeSpacing>
+                            <FavIcon />
+                        </Link>
+                    </div>
+                    <div>
+                        <span className="flex items-center gap-2">
+                            Estado
+                            <CircleIcon width="w-2" fill="fill-green-400" stroke="stroke-green-400" />
+                            <span className="text-green-400">Normal</span>
+                        </span>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-8 items-end">
                     <div>
                         <ContactSocial />
                     </div>
                     <div>
-                        <ul className="flex gap-2">
-                            {navLinks.map((link: LinkTypes) => {
-                                const { id } = link;
-                                return <NavLink key={id} link={link} />;
-                            })}
-                        </ul>
+                        <NavItems />
                     </div>
                 </div>
             </div>
