ab3681f96a83a37506c13e4589636c744393cba0
Jeremy Díaz <jeremy664k@gmail.com>
2023-03-07 20:05:55 -0300

    Add onclick prop in link

--- src/elements/Link.tsx
+++ src/elements/Link.tsx
@@ -1,14 +1,14 @@
 import { INTERACTIVE_CLASS, INTERACTIVE_ACTIVE_CLASS } from "../constants/stylesClass";
 import { type LinkTypesProps } from "../types";
 
-export const Link: React.FC<LinkTypesProps> = ({ href, title, target, removeSpacing, isActive, children }) => {
+export const Link: React.FC<LinkTypesProps> = ({ href, title, target, removeSpacing, isActive, onClick, children }) => {
     const activeClass = isActive ? INTERACTIVE_ACTIVE_CLASS : "";
     const removeSpacingClass = !removeSpacing ? "py-2 px-3" : "";
 
     const linkClass = `${activeClass} ${removeSpacingClass} ${INTERACTIVE_CLASS}`.trim();
 
     return (
-        <a className={linkClass} href={href} title={title} target={target} rel="noopener noreferrer">
+        <a className={linkClass} href={href} title={title} target={target} onClick={onClick} rel="noopener noreferrer">
             {children}
         </a>
     );
