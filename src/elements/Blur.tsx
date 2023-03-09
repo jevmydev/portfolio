5ae12ac3543a107669dc0334de421dbd3b32b262
Jeremy Díaz <jeremy664k@gmail.com>
2023-03-09 19:18:11 -0300

    Add element blur

---
+++ src/elements/Blur.tsx
@@ -0,0 +1,10 @@
+interface BlurProps {
+    isFullScreen?: boolean;
+}
+
+export const Blur = ({ isFullScreen }: BlurProps) => {
+    const blurFullScreenClass = isFullScreen ? "w-full p-10" : "pt-32";
+    return <div className={`${blurFullScreenClass} absolute -z-10 blur-3xl opacity-20 bg-gradient-to-r from-primary-purple to-primary-pink`}></div>;
+};
+
+export default Blur;
