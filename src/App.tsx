4ba69205e402870711b04cc0f5cc7159cfc33fb8
Jeremy Díaz <jeremy664k@gmail.com>
2023-03-09 19:19:59 -0300

    Feat click sound and blur element

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Projects from "./components/Projects/Projects";
@@ -7,10 +8,23 @@ import Footer from "./components/Footer/Footer";
 
 import SkillsSlider from "./components/Skills/SkillsSlider";
 import ProjectsMore from "./components/Projects/ProjectsMore";
+import Blur from "./elements/Blur";
 
 const App = () => {
+    useEffect(() => {
+        const clickSoundEvent = () => {
+            const audio = new Audio("../public/assets/sounds/click.mp3");
+            audio.volume = 0.09;
+            audio.play();
+        };
+
+        window.addEventListener("click", clickSoundEvent);
+        return () => window.removeEventListener("click", clickSoundEvent);
+    }, []);
+
     return (
         <>
+            <Blur isFullScreen />
             <Header />
             <main>
                 <Hero />
