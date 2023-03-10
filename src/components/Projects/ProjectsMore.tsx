46c6eb1f15ee71f42ffeb353663bd0cde872db0a
Jeremy Díaz <jeremy664k@gmail.com>
2023-03-10 18:27:24 -0300

    Update container with a div

--- src/components/Projects/ProjectsMore.tsx
+++ src/components/Projects/ProjectsMore.tsx
@@ -2,10 +2,12 @@ import Link from "../../elements/Link";
 
 export const ProjectsMore = () => {
     return (
-        <div className="max-w-screen-xl mx-auto pt-28 p-4 flex">
-            <Link href="https://github.com/jevmydev?tab=repositories" title="Revisa otros proyectos en Github" target="_blank" isActive>
-                ¡Revisa otros proyectos!
-            </Link>
+        <div>
+            <div className="max-w-screen-xl mx-auto pt-28 p-4 flex">
+                <Link href="https://github.com/jevmydev?tab=repositories" title="Revisa otros proyectos en Github" target="_blank" isActive>
+                    ¡Revisa otros proyectos!
+                </Link>
+            </div>
         </div>
     );
 };
