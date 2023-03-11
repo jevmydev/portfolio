85c45d34bac6012452e71fbf9c0445f89f2c8cd7
Jeremy Díaz <jeremy664k@gmail.com>
2023-03-11 14:14:10 -0300

    Remove unused animation value

--- src/constants/defaultsValues.ts
+++ src/constants/defaultsValues.ts
@@ -1,6 +1,5 @@
 export const DEFAULT_FORM_STATUS = { message: "", isError: false };
 export const DEFAULT_PREFERENCE = {
-    animation: true,
-    sound: true,
+    sound: false,
     scheme: window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light"
 };
