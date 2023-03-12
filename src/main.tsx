7ea2f06559e78fc98a611906cefaec74169434e3
Jeremy Díaz <jeremy664k@gmail.com>
2023-03-12 16:17:19 -0300

    Add preference context provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 
 import App from "./App";
+import { PreferenceProvider } from "./context/preference";
 
 import "./main.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <App />
+        <PreferenceProvider>
+            <App />
+        </PreferenceProvider>
     </React.StrictMode>
 );
