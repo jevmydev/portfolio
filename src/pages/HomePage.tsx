3d8d4ffff322dab31d91d2da4c50cf4375cda49c
Jeremy Díaz <jeremy664k@gmail.com>
2023-03-12 21:19:29 -0300

    Add skills slider after About

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -18,6 +18,7 @@ const HomePage = () => {
                 <SkillsSlider />
                 <Projects />
                 <About />
+                <SkillsSlider />
                 <Contact />
             </main>
             <Footer />
