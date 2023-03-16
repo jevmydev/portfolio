b660c333dfeadeb8e15e7dcc5694e8fcc2b02aa2
Jeremy Díaz <jeremy664k@gmail.com>
2023-03-15 21:05:32 -0300

    Add font-medium for link, stat and strong

--- src/components/About/AboutStatsItem.tsx
+++ src/components/About/AboutStatsItem.tsx
@@ -4,7 +4,7 @@ interface AboutStatsItemProps {
 }
 
 export const AboutStatsItem = ({ stat, description }: AboutStatsItemProps) => {
-    const statClass = "text-5xl text-slate-800 dark:text-slate-200";
+    const statClass = "font-medium text-5xl text-slate-800 dark:text-slate-200";
 
     return (
         <div className="flex flex-col gap-2 p-4 border-2 border-slate-400/20 rounded">
