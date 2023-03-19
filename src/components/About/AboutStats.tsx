9f6c4922a6867394ce66622ce4e6fa824bda5844
Jeremy Díaz <jeremy664k@gmail.com>
2023-03-19 16:48:09 -0300

    Update contributions number

--- src/components/About/AboutStats.tsx
+++ src/components/About/AboutStats.tsx
@@ -5,7 +5,7 @@ export const AboutStats = () => {
         <div className="flex flex-wrap gap-4 lg:justify-end">
             <AboutStatsItem stat="2+" description="Años Desarrollando" />
             <AboutStatsItem stat="24+" description="Repositorios" />
-            <AboutStatsItem stat="440+" description="Contribuciones" />
+            <AboutStatsItem stat="500+" description="Contribuciones" />
             <AboutStatsItem stat="99.99%" description="Focus" />
         </div>
     );
