bd8e22e716f604da6e8d5f3fb7209f0f2840f56a
Jeremy Díaz <jeremy664k@gmail.com>
2023-03-22 20:12:01 -0300

    Fix error gramatical

--- src/components/About/AboutStats.tsx
+++ src/components/About/AboutStats.tsx
@@ -3,7 +3,7 @@ import AboutStatsItem from "./AboutStatsItem";
 export const AboutStats = () => {
     return (
         <div className="flex flex-wrap gap-4 lg:justify-end">
-            <AboutStatsItem stat="2+" description="Años Desarrollando" />
+            <AboutStatsItem stat="2+" description="Años desarrollando" />
             <AboutStatsItem stat="24+" description="Repositorios" />
             <AboutStatsItem stat="500+" description="Contribuciones" />
             <AboutStatsItem stat="99.99%" description="Focus" />
