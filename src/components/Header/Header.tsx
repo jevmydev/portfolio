0a10eec23a7355e7e7af1c31682a27b9a16117be
Jeremy Díaz <jeremy664k@gmail.com>
2023-03-25 23:08:24 -0300

    Add animate fadeIn

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ import Nav from "./Nav";
 
 export const Header = () => {
     return (
-        <header className="fixed z-20 w-full backdrop-blur-md">
+        <header className="fixed z-20 w-full backdrop-blur-md animate-fadeIn">
             <div className="max-w-screen-xl mx-auto p-4 flex items-center justify-between">
                 <div className="relative z-30">
                     <Link href="/" title="Icono de Jeremy Díaz" removeSpacing>
