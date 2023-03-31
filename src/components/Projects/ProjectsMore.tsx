47ec39e18ba1b45d3e3b854df56735ebfef67bf4
Jeremy Díaz <jeremy664k@gmail.com>
2023-03-31 17:54:54 -0300

    Update responsive blur

--- src/components/Projects/ProjectsMore.tsx
+++ src/components/Projects/ProjectsMore.tsx
@@ -4,10 +4,12 @@ import Link from "../../elements/Link";
 export const ProjectsMore = () => {
     return (
         <div className="flex">
+            <div className="hidden sm:block">
+                <Blur />
+            </div>
             <Link href="https://github.com/jevmydev?tab=repositories" title="Revisa otros proyectos en Github" target="_blank" isActive>
                 ¡Revisa otros proyectos!
             </Link>
-            <Blur />
         </div>
     );
 };
