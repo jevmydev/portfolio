544a69ee632356f7d8a8dd6b43f5983238f48a12
Jeremy Díaz <jeremy664k@gmail.com>
2023-03-31 17:39:10 -0300

    Refactor stat prop

--- src/components/About/AboutStats.tsx
+++ src/components/About/AboutStats.tsx
@@ -3,9 +3,9 @@ import AboutStatsItem from "./AboutStatsItem";
 export const AboutStats = () => {
     return (
         <div className="flex flex-wrap gap-4 lg:justify-end">
-            <AboutStatsItem stat="2+" description="Años desarrollando" />
-            <AboutStatsItem stat="24+" description="Repositorios" />
-            <AboutStatsItem stat="500+" description="Contribuciones" />
+            <AboutStatsItem stat="+2" description="Años desarrollando" />
+            <AboutStatsItem stat="+24" description="Repositorios" />
+            <AboutStatsItem stat="+500" description="Contribuciones" />
             <AboutStatsItem stat="99.99%" description="Focus" />
         </div>
     );
