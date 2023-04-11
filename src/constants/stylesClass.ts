9354a4e7f3696d69b71b279254147617bab2674b
Jeremy Díaz <jeremy664k@gmail.com>
2023-04-10 20:10:39 -0400

    Update interactive active and submit styles

--- src/constants/stylesClass.ts
+++ src/constants/stylesClass.ts
@@ -1,5 +1,5 @@
 export const INTERACTIVE_CLASS = "font-medium flex gap-2 items-center justify-start transition rounded hover:text-slate-800 hover:bg-slate-400/20 dark:hover:text-slate-200 active:scale-95" as const;
-export const INTERACTIVE_ACTIVE_CLASS = "text-primary-blue bg-slate-800 hover:bg-slate-400/20" as const;
+export const INTERACTIVE_ACTIVE_CLASS = "text-primary-blue saturate-200 bg-slate-800 hover:bg-slate-400/20" as const;
 
 export const GRADIENT_CLASS = "text-transparent bg-clip-text bg-gradient-to-r from-primary-purple to-primary-pink" as const;
 
@@ -9,7 +9,7 @@ export const SUBTITLE_COLOR_CLASS = `${GRADIENT_CLASS} tracking-tight font-bold 
 
 export const INPUT_CLASS = "w-full outline-none py-2 px-4 bg-transparent text-slate-800 dark:text-slate-200 border-2 border-slate-400/20 rounded transition-colors focus:bg-slate-400/20" as const;
 
-export const SUBMIT_INPUT_CLASS = `${INTERACTIVE_CLASS} ${INTERACTIVE_ACTIVE_CLASS} py-2 px-4 cursor-pointer` as const;
+export const SUBMIT_INPUT_CLASS = `${INTERACTIVE_CLASS} ${INTERACTIVE_ACTIVE_CLASS} py-2 px-4 cursor-pointer justify-center w-full sm:w-auto sm:justify-start` as const;
 
 export const SLIDER_SHADOW_CLASS =
     "before:bg-gradient-to-r before:from-semi-white dark:before:from-semi-dark before:to-transparent before:absolute before:z-10 before:w-24 before:h-full before:left-0 before:top-0 after:bg-gradient-to-l after:from-semi-white dark:after:from-semi-dark after:to-transparent after:absolute after:z-10 after:w-24 after:h-full after:right-0 after:top-0" as const;
