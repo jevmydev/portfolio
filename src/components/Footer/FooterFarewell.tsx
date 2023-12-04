d4f61233269072b9c0f8e47c0819a5620fd72f89
Jeremy Díaz <jeremy664k@gmail.com>
2023-12-04 18:12:33 -0300

    Add paragraph farewell of return message

--- src/components/Footer/FooterFarewell.tsx
+++ src/components/Footer/FooterFarewell.tsx
@@ -1,10 +1,17 @@
+import { GRADIENT_CLASS } from "../../constants/stylesClass";
+
 export const FooterFarewell = () => {
     const date = new Date();
     const formattedDate = new Intl.DateTimeFormat("es-ES", {
         weekday: "long"
     }).format(date);
 
-    return <span className="dark:text-primary-blue">Feliz {formattedDate}</span>;
+    return (
+        <p>
+            <span className="dark:text-primary-blue block">Feliz {formattedDate}</span>
+            <span className={`font-bold ${GRADIENT_CLASS}`}>Return: 04-12-2023</span>
+        </p>
+    );
 };
 
 export default FooterFarewell;
