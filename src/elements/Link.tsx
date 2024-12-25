5978fc276e099e5ea7b7cc1cc9711500a71b35e3
Jeremy Díaz <jeremy664k@gmail.com>
2024-12-25 20:50:43 -0300

    Add center class

--- src/elements/Link.tsx
+++ src/elements/Link.tsx
@@ -1,11 +1,12 @@
 import { INTERACTIVE_CLASS, INTERACTIVE_ACTIVE_CLASS } from "../constants/stylesClass";
 import { type LinkTypesProps } from "../types";
 
-export const Link: React.FC<LinkTypesProps> = ({ href, title, target, removeSpacing, isActive, onClick, children }) => {
-    const activeClass = isActive ? INTERACTIVE_ACTIVE_CLASS : "";
+export const Link: React.FC<LinkTypesProps> = ({ href, title, target, removeSpacing, isActive, isCenter, onClick, children }) => {
+    const activeClass = isActive ? INTERACTIVE_ACTIVE_CLASS : "hover:text-slate-800";
+    const centerClass = isCenter ? "justify-center" : "justify-start";
     const removeSpacingClass = !removeSpacing ? "py-2 px-3" : "";
 
-    const linkClass = `${activeClass} ${removeSpacingClass} ${INTERACTIVE_CLASS}`.trim();
+    const linkClass = `${activeClass} ${centerClass} ${removeSpacingClass} ${INTERACTIVE_CLASS}`.trim();
 
     return (
         <a className={linkClass} href={href} title={title} target={target} rel="noopener noreferrer" onClick={onClick}>
