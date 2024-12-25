c081229424a33cd2733a7ae324e54dfedfef1e76
Jeremy Díaz <jeremy664k@gmail.com>
2024-12-25 20:49:12 -0300

    Add grid of boards design and update time of dev

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -10,13 +10,14 @@ export const Hero = () => {
     return (
         <section id="hero">
             <div className="animate-fadeIn max-w-screen-xl mx-auto pt-20 p-4 h-screen flex flex-col gap-8 justify-center items-center text-center">
+                <div className="absolute -z-10 h-full w-full left-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:60px_60px] [mask-image:radial-gradient(ellipse_60%_80%_at_50%_0%,#000_10%,transparent_100%)]"></div>
                 <div className="flex flex-col gap-2">
                     <h1 className={titleClass}>Jeremy Díaz</h1>
                     <h2 className={subtitleClass}>Frontend Developer</h2>
                 </div>
                 <div className="max-w-md">
                     <p>
-                        Llevo más de dos años desarrollando experiencias de usuario atractivas y funcionales. <span className="animate-greeting inline-block">👋</span>
+                        Llevo más de tres años desarrollando experiencias de usuario atractivas y funcionales. <span className="animate-greeting inline-block">👋</span>
                     </p>
                 </div>
                 <div className="flex gap-2">
