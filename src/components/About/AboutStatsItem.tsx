839c385acd271aa3e184f5de8999329645df8f19
Jeremy Díaz <jeremy664k@gmail.com>
2024-12-25 20:48:09 -0300

    update design for stats item

--- src/components/About/AboutStatsItem.tsx
+++ src/components/About/AboutStatsItem.tsx
@@ -1,16 +1,20 @@
 interface AboutStatsItemProps {
     stat: string;
     description: string;
+    isFullWidth?: boolean;
 }
 
-export const AboutStatsItem = ({ stat, description }: AboutStatsItemProps) => {
+export const AboutStatsItem = ({ stat, description, isFullWidth }: AboutStatsItemProps) => {
     const statClass = "font-medium text-5xl text-black dark:text-white";
+    const isFullWidthClass = isFullWidth ? "col-span-2" : "";
 
     return (
-        <div className="flex flex-col gap-2 p-4 border-2 border-slate-400/20 rounded">
-            <span className={statClass}>{stat}</span>
-            <span>{description}</span>
-        </div>
+        <>
+            <div className={`${isFullWidthClass} w-full flex items-center flex-col gap-2 p-4 border-2 border-slate-400/20 rounded`}>
+                <span className={statClass}>{stat}</span>
+                <span className="whitespace-nowrap">{description}</span>
+            </div>
+        </>
     );
 };
 
